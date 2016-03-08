22ebacf3df8566001049fb7b8d80c40654896296
Štěpán Beneš <benes.stepan@gmail.com>
2016-03-08 12:13:06 +0100

    Generate sequence of triangle faces for all linear cell types in mesh face generator

--- MeshEditor.LayerManager/MeshFaceGenerator.cs
+++ MeshEditor.LayerManager/MeshFaceGenerator.cs
@@ -9,7 +9,9 @@ namespace MeshEditor.LayerManager
 {
 	class MeshFaceGenerator
 	{
-		public struct TriangleFace
+		#region Static members
+
+		private struct TriangleFace
 		{
 			public int Point1Id { get; }
 			public int Point2Id { get; }
@@ -22,11 +24,19 @@ namespace MeshEditor.LayerManager
 			}
 		}
 
+		#endregion
+
+		#region Properties
+
 		public int NumberOfTriangles => TriangleConnectivity.Length / 3;
 		public int NumberOfEdges => EdgeConnectivity.Length / 2;
 
 		public int[] TriangleConnectivity { get; private set; }
 		public int[] EdgeConnectivity { get; private set; }
+
+		#endregion
+
+		#region Public methods
 
 		public void ProcessGeometry(GeometryDescription geometry)
 		{
@@ -34,39 +44,8 @@ namespace MeshEditor.LayerManager
 			int pointIndex = 0;
 			for (int i = 0; i < geometry.NumberOfCells; i++)
 			{
-				switch (geometry.CellTypes[i])
-				{
-					case CellType.Point:
-						continue;
-					case CellType.LineLinear:
-						throw new NotImplementedException();
-					case CellType.LineQuadratic:
-						throw new NotImplementedException();
-					case CellType.TriangleLinear:
-						triangleFaces.Add(new TriangleFace(geometry.CellConnectivity[pointIndex], geometry.CellConnectivity[pointIndex + 1], geometry.CellConnectivity[pointIndex + 2]));
-						pointIndex += 3;
-						break;
-					case CellType.TriangleQuadratic:
-						throw new NotImplementedException();
-					case CellType.QuadLinear:
-						throw new NotImplementedException();
-					case CellType.QuadQuadratic:
-						throw new NotImplementedException();
-					case CellType.TetraLinear:
-						throw new NotImplementedException();
-					case CellType.TetraQuadratic:
-						throw new NotImplementedException();
-					case CellType.WedgeLinear:
-						throw new NotImplementedException();
-					case CellType.WedgeQuadratic:
-						throw new NotImplementedException();
-					case CellType.HexaLinear:
-						throw new NotImplementedException();
-					case CellType.HexaQuadratic:
-						throw new NotImplementedException();
-					default:
-						throw new NotSupportedException();
-				}
+				triangleFaces.AddRange(GetSequenceOfFaces(geometry.CellTypes[i], geometry.CellConnectivity, pointIndex));
+				pointIndex += mapCellTypeToNumberOfPoints(geometry.CellTypes[i]);
 			}
 
 			TriangleConnectivity = new int[triangleFaces.Count * 3];
@@ -80,5 +59,206 @@ namespace MeshEditor.LayerManager
 
 			EdgeConnectivity = new int[0];
 		}
+
+		#endregion
+
+		#region Private methods
+
+		private static int mapCellTypeToNumberOfPoints(CellType cellType)
+		{
+			switch (cellType)
+			{
+				case CellType.Point:
+					return 1;
+				case CellType.LineLinear:
+					return 2;
+				case CellType.LineQuadratic:
+					return 3;
+				case CellType.TriangleLinear:
+					return 3;
+				case CellType.TriangleQuadratic:
+					return 6;
+				case CellType.QuadLinear:
+					return 4;
+				case CellType.QuadQuadratic:
+					return 8;
+				case CellType.TetraLinear:
+					return 4;
+				case CellType.TetraQuadratic:
+					return 10;
+				case CellType.WedgeLinear:
+					return 6;
+				case CellType.WedgeQuadratic:
+					return 15;
+				case CellType.HexaLinear:
+					return 8;
+				case CellType.HexaQuadratic:
+					return 20;
+				default:
+					throw new NotSupportedException();
+			}
+		}
+
+		private static IEnumerable<TriangleFace> GetSequenceOfFaces(CellType cellType, int[] cellConnectivity, int startIndex)
+		{
+			// depends on face ordering!
+			switch (cellType)
+			{
+				case CellType.Point:
+				case CellType.LineLinear:
+				case CellType.LineQuadratic:
+					yield break; // point and beam has no face
+				case CellType.TriangleLinear:
+					yield return new TriangleFace(
+						cellConnectivity[startIndex],
+						cellConnectivity[startIndex + 1],
+						cellConnectivity[startIndex + 2]);
+					break;
+				case CellType.TriangleQuadratic:
+
+					throw new NotImplementedException();
+
+				case CellType.QuadLinear:
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 0],
+						cellConnectivity[startIndex + 1],
+						cellConnectivity[startIndex + 2]);
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 0],
+						cellConnectivity[startIndex + 2],
+						cellConnectivity[startIndex + 3]);
+					break;
+				case CellType.QuadQuadratic:
+
+					throw new NotImplementedException();
+
+				case CellType.TetraLinear:
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 2],
+						cellConnectivity[startIndex + 1],
+						cellConnectivity[startIndex + 3]);
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 0],
+						cellConnectivity[startIndex + 2],
+						cellConnectivity[startIndex + 3]);
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 1],
+						cellConnectivity[startIndex + 0],
+						cellConnectivity[startIndex + 3]);
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 0],
+						cellConnectivity[startIndex + 1],
+						cellConnectivity[startIndex + 2]);
+					break;
+				case CellType.TetraQuadratic:
+
+					throw new NotImplementedException();
+
+				case CellType.WedgeLinear:
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 0],
+						cellConnectivity[startIndex + 1],
+						cellConnectivity[startIndex + 2]);
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 3],
+						cellConnectivity[startIndex + 5],
+						cellConnectivity[startIndex + 4]);
+
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 1],
+						cellConnectivity[startIndex + 0],
+						cellConnectivity[startIndex + 3]);
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 1],
+						cellConnectivity[startIndex + 3],
+						cellConnectivity[startIndex + 4]);
+
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 2],
+						cellConnectivity[startIndex + 1],
+						cellConnectivity[startIndex + 4]);
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 2],
+						cellConnectivity[startIndex + 4],
+						cellConnectivity[startIndex + 5]);
+
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 0],
+						cellConnectivity[startIndex + 2],
+						cellConnectivity[startIndex + 5]);
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 0],
+						cellConnectivity[startIndex + 5],
+						cellConnectivity[startIndex + 3]);
+					break;
+				case CellType.WedgeQuadratic:
+
+					throw new NotImplementedException();
+
+				case CellType.HexaLinear:
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 0],
+						cellConnectivity[startIndex + 3],
+						cellConnectivity[startIndex + 7]);
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 0],
+						cellConnectivity[startIndex + 7],
+						cellConnectivity[startIndex + 4]);
+
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 1],
+						cellConnectivity[startIndex + 0],
+						cellConnectivity[startIndex + 4]);
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 1],
+						cellConnectivity[startIndex + 4],
+						cellConnectivity[startIndex + 5]);
+
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 2],
+						cellConnectivity[startIndex + 1],
+						cellConnectivity[startIndex + 5]);
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 2],
+						cellConnectivity[startIndex + 5],
+						cellConnectivity[startIndex + 6]);
+
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 3],
+						cellConnectivity[startIndex + 2],
+						cellConnectivity[startIndex + 6]);
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 3],
+						cellConnectivity[startIndex + 6],
+						cellConnectivity[startIndex + 7]);
+
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 0],
+						cellConnectivity[startIndex + 1],
+						cellConnectivity[startIndex + 2]);
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 0],
+						cellConnectivity[startIndex + 2],
+						cellConnectivity[startIndex + 3]);
+
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 4],
+						cellConnectivity[startIndex + 5],
+						cellConnectivity[startIndex + 6]);
+					yield return new TriangleFace(
+						cellConnectivity[startIndex + 4],
+						cellConnectivity[startIndex + 6],
+						cellConnectivity[startIndex + 7]);
+					break;
+				case CellType.HexaQuadratic:
+
+					throw new NotImplementedException();
+
+				default:
+					throw new NotSupportedException();
+			}
+		}
+
+		#endregion
 	}
 }
