a56fc44092e0043405496ba364e4000899efca4f
Štěpán Beneš <benes.stepan@gmail.com>
2017-09-04 19:14:57 +0200

    Small adjustment in layout of DeformationFilterForm

--- MeshEditor.DataVisualizer/UI/DeformationFilterParamsForm.Designer.cs
+++ MeshEditor.DataVisualizer/UI/DeformationFilterParamsForm.Designer.cs
@@ -42,7 +42,7 @@
 			// buttonOK
 			// 
 			this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.buttonOK.Location = new System.Drawing.Point(387, 248);
+			this.buttonOK.Location = new System.Drawing.Point(344, 248);
 			this.buttonOK.Name = "buttonOK";
 			this.buttonOK.Size = new System.Drawing.Size(100, 28);
 			this.buttonOK.TabIndex = 0;
@@ -54,7 +54,7 @@
 			// 
 			this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.buttonCancel.Location = new System.Drawing.Point(493, 248);
+			this.buttonCancel.Location = new System.Drawing.Point(450, 248);
 			this.buttonCancel.Name = "buttonCancel";
 			this.buttonCancel.Size = new System.Drawing.Size(100, 28);
 			this.buttonCancel.TabIndex = 1;
@@ -65,24 +65,24 @@
 			// 
 			this.comboBoxDeformationField.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.comboBoxDeformationField.FormattingEnabled = true;
-			this.comboBoxDeformationField.Location = new System.Drawing.Point(12, 41);
+			this.comboBoxDeformationField.Location = new System.Drawing.Point(11, 29);
 			this.comboBoxDeformationField.Name = "comboBoxDeformationField";
-			this.comboBoxDeformationField.Size = new System.Drawing.Size(230, 24);
+			this.comboBoxDeformationField.Size = new System.Drawing.Size(249, 24);
 			this.comboBoxDeformationField.TabIndex = 2;
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(9, 21);
+			this.label1.Location = new System.Drawing.Point(8, 9);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(130, 17);
+			this.label1.Size = new System.Drawing.Size(234, 17);
 			this.label1.TabIndex = 3;
-			this.label1.Text = "Displacements field";
+			this.label1.Text = "Field with displacements information";
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(9, 168);
+			this.label2.Location = new System.Drawing.Point(8, 148);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(159, 17);
 			this.label2.TabIndex = 4;
@@ -90,9 +90,9 @@
 			// 
 			// textBoxLayerName
 			// 
-			this.textBoxLayerName.Location = new System.Drawing.Point(12, 188);
+			this.textBoxLayerName.Location = new System.Drawing.Point(11, 168);
 			this.textBoxLayerName.Name = "textBoxLayerName";
-			this.textBoxLayerName.Size = new System.Drawing.Size(230, 22);
+			this.textBoxLayerName.Size = new System.Drawing.Size(249, 22);
 			this.textBoxLayerName.TabIndex = 5;
 			this.textBoxLayerName.Text = "deformation";
 			// 
@@ -101,11 +101,11 @@
 			this.trackBarScale.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.trackBarScale.LargeChange = 10;
-			this.trackBarScale.Location = new System.Drawing.Point(12, 108);
+			this.trackBarScale.Location = new System.Drawing.Point(11, 88);
 			this.trackBarScale.Margin = new System.Windows.Forms.Padding(4);
 			this.trackBarScale.Maximum = 100;
 			this.trackBarScale.Name = "trackBarScale";
-			this.trackBarScale.Size = new System.Drawing.Size(580, 56);
+			this.trackBarScale.Size = new System.Drawing.Size(537, 56);
 			this.trackBarScale.TabIndex = 6;
 			this.trackBarScale.Value = 10;
 			this.trackBarScale.ValueChanged += new System.EventHandler(this.trackBarScale_ValueChanged);
@@ -113,7 +113,7 @@
 			// labelScale
 			// 
 			this.labelScale.AutoSize = true;
-			this.labelScale.Location = new System.Drawing.Point(9, 87);
+			this.labelScale.Location = new System.Drawing.Point(8, 67);
 			this.labelScale.Name = "labelScale";
 			this.labelScale.Size = new System.Drawing.Size(43, 17);
 			this.labelScale.TabIndex = 7;
@@ -125,7 +125,7 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.buttonCancel;
-			this.ClientSize = new System.Drawing.Size(605, 288);
+			this.ClientSize = new System.Drawing.Size(562, 288);
 			this.Controls.Add(this.labelScale);
 			this.Controls.Add(this.trackBarScale);
 			this.Controls.Add(this.textBoxLayerName);
